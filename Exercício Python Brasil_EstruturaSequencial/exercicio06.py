c21558edd9171d82dfeaa755c3fd7e723ac1769d
Leandro Varela <leandro_varela@live.com>
2022-02-11 15:43:15 -0300

    Modificando o código de pi com a biblioteca math

--- Exercício Python Brasil_EstruturaSequencial/exercicio06.py
+++ Exercício Python Brasil_EstruturaSequencial/exercicio06.py
@@ -1,10 +1,11 @@
+import math
 def calculate_ray():
     
     print("------------------Execício 06------------------\n")
     
     raio = float(input("Insira o valor do raio: "))
     
-    area= 3.14 *(raio ** 2)
+    area= math.pi *(raio ** 2)
     
     
     print(f"\nO valor da área desse círculo é {area}cm²")
