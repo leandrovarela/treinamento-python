0eb4aad103d4d356dcbdb445d566a67d286e6b5e
Leandro Varela <leandro_varela@live.com>
2022-02-17 14:35:32 -0300

    Adicionando ex06 Otimizado

--- Exercicios Python Brasil_ EstruturasDDecisão/ex06.py
+++ Exercicios Python Brasil_ EstruturasDDecisão/ex06.py
@@ -2,18 +2,14 @@ def big_num():
    print("\n---------------Exercício 06-----------------")
    
    num = []
+   for numero in range (3):
+      num.append(int(input(f"\nInsira o {1+numero}º valor: ")))
    
-   num_01=int(input("\nInsira um valor: "))
-   num.append(num_01)
-   num_02=int(input("\nInsira um segundo valor: "))
-   num.append(num_02)
-   num_03=int(input("\nInsira um teceiro valor: "))
-   num.append(num_03)
    
    maior= max(num)
    
    print(f"\n O maior numero inserido é {maior}")
 
-
+big_num()
 
 
