855250d01fe6bcdc5d44f147dd171a502e162c86
Leandro Varela <leandro_varela@live.com>
2022-03-09 16:50:52 -0300

    Adicionando Lista 05 de Funções

--- Python FuncoesSoluucoes/ex06.py
+++ Python FuncoesSoluucoes/ex06.py
@@ -1,7 +1,7 @@
-def converterData(hora, minuto, indicador):
+"""def converterData(hora, minuto, indicador):
     hour = hora - 12 
     min = minuto
     indicador= "PM" if hora > 12 
      else: "AM"
 
-def parse( hora , minuto):
+def parse( hora , minuto):"""
--- Python FuncoesSoluucoes/main.py
+++ Python FuncoesSoluucoes/main.py
@@ -4,15 +4,15 @@ import ex02
 import ex03
 import ex04
 import ex05
-import ex06
-import ex07
-import ex08
-import ex09
-import ex10
-import ex11
-import ex12
-import ex13
-import ex14
+#import ex06
+#import ex07
+#import ex08
+#import ex09
+#import ex10
+#import ex11
+#import ex12
+#import ex13
+#import ex14
 
 
 
