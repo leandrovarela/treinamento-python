76cf3c144b7b9b4a98d2261300887100dc5e84a5
Leandro Varela <leandro_varela@live.com>
2022-04-06 15:38:23 -0300

    Melhorias Lista 05 e 06

--- ExerciciosPython_Strings/Ex07.py
+++ ExerciciosPython_Strings/Ex07.py
@@ -1,16 +1,13 @@
+def contador_de_vogais_e_espaços():
+    quantVogais= 0
+    frase = input('Digite a frase: ')
+    espaco = 0
+    for indice in frase:
+        print(indice)
+        if indice == " ":
+            espaco += 1
+        if indice in "aeiou":
+            quantVogais += 1
 
-
-
-vogais=["a,e,i,o,u"]
-quantVogais= 0
-frase = input('Digite a frase: ')
-branco = 0
-for indice in frase:
-    print(indice)
-    if indice == " ":
-        branco += 1
-    if indice in "aeiou":
-        quantVogais += 1
-
-print('Total de espaços em branco: %d' % branco)
-print('Total de vogais: %d' % quantVogais)
+    print('Total de espaços em branco: %d' % espaco)
+    print('Total de vogais: %d' % quantVogais)
