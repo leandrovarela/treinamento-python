f211482658297375b3ef5e3aae34d2596bae3a02
Leandro Varela <leandro_varela@live.com>
2022-04-06 09:08:11 -0300

    Melhorias Lista 04 de Listas

--- Exercicios_PythonBrasil_Listas/ex22.py
+++ Exercicios_PythonBrasil_Listas/ex22.py
@@ -1,4 +1,26 @@
-def par_impar():
-    print("\n---------------Exercício 22-----------------")
+def relatorio_reparo_mouses():
 
+    problemas = [0]*4
+    print('''   1- necessita da esfera
+    2- necessita de limpeza
+    3- necessita troca do cabo ou conector
+    4- quebrado ou inutilizado ''')
+    while True:
+        identificacao = int(input('Identificação: '))
+        if identificacao == 0:
+            break
+        problema = int(input('Digite o número do problema: '))
     
+        problemas[problema-1] += 1
+
+    print(problemas)    
+    total = sum(problemas)
+    print(f"\n Quantidades de Mouses {total} \n")               
+    print('Situação                        Quantidade              Percentual')
+    
+    print('1- Necessita da esfera                  %d              %.2f' % (problemas[0],  (100 * problemas[0]) / total))
+    print('2- Necessita de limpeza                 %d              %.2f' % (problemas[1], (100 * problemas[1]) / total))
+    print('3- Necessita troca do cabo ou conector  %d              %.2f' % (problemas[2], (100 * problemas[2]) / total))
+    print('4- Quebrado ou inutilizado              %d              %.2f' % (problemas[3], (100 * problemas[3]) / total))
+    
+relatorio_reparo_mouses()
