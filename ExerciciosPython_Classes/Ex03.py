0695ffeb264c51e2af7850e741e7f1a5731c9112
Leandro Varela <leandro_varela@live.com>
2022-04-13 15:42:08 -0300

    Melhorias Lista 08

--- ExerciciosPython_Classes/Ex03.py
+++ ExerciciosPython_Classes/Ex03.py
@@ -12,7 +12,7 @@ class Retangulo:
         return new_largura
 
     def mostra_medidas(self):
-        return f"As medidas do Retângulo são: Comprimento {self.comprimento}cm,Largura {self.largura}cm."
+        return f"As medidas do Retângulo são: Comprimento {self.comprimento}cm, Largura {self.largura}cm."
 
     def calcula_area(self):
         area= self.comprimento * self.largura
@@ -21,6 +21,7 @@ class Retangulo:
     def calcula_perimetro(self):
         perimetro= 2*(self.largura + self.comprimento)
         return perimetro
+
     def informa_gastos(self):
         return f"Serão necessários {self.calcula_area()}m² quadrado(s) de piso e {self.calcula_perimetro()}m de rodapés para suprir o local."
         
@@ -29,6 +30,12 @@ x =float(input("Insira o comprimento: "))
 y =float(input("Insira a largura: "))
 
 a = Retangulo(x,y)
+
+print(a.informa_gastos())
+print(a.mostra_medidas())
+print(a.altera_comprimento(8.0))
+print(a.altera_largura(7.0))
+print(a.mostra_medidas())
 print(a.informa_gastos())
 
  
